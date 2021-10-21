d11583f64062c5a49514ef4696b70edeb3120b70
benoitgraziani <benoit.c.graziani@hotmail.fr>
2021-10-21 09:36:03 +0200

    e

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: "articles#index"
-  resources :articles, except: :index
+  resources :articles, except: :index do 
+    collection do
+      get :element
+    end
+    end
+  end
   # root to: "pages#home"
 end
